8eb042f344b7a263f9c18548ee1bc3dc70fa4f79
Andrew <brendagary@Brendas-MacBook-Pro.local>
2023-03-01 03:14:49 -0600

    created an animation for flipping the card when a user clicks to see the other side

--- components/StudyCards.tsx
+++ components/StudyCards.tsx
@@ -1,28 +1,44 @@
 import React, {useEffect, useState} from 'react'
+import {motion} from 'framer-motion';
 
 
 type Props = {}
 
-const StudyCards = (props: Props) => {
+const StudyCards = ({allCards}) => {
 
-    const [allCards, setAllCards] = useState([])
-    useEffect(() => {
-        const useEffectAsync = async () => {
-            const resp = await fetch('/api/cards');
+  const [frontOrBack, setFrontOrBack] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-            const allCardsResp = await resp.json();
-
-            // console.log('allCards: ', allCards);
-            setAllCards(allCardsResp);
-        }
-
-        useEffectAsync();
-    }, [])
+  const changeSides = () => {
+    setFrontOrBack(!frontOrBack);
+  }
+    
   return (
-    <div className='w-full h-80 flex justify-center items-center'>
-        <div className='h-full w-2/3 flex flex-col border border-green-500' >
-        </div>
-    </div>
+    <motion.div animate={{rotateY: frontOrBack ? 0 : 360}} transition={{duration: .4}} className='relative w-1/2 border border-black flex items-center h-80 rounded-lg justify-center' onClick={(e) => {
+      e.preventDefault();
+      changeSides();
+    }}>
+      <div className='absolute left-1 top-1'>
+        {currentIndex + 1}/{allCards.length}
+      </div>
+      <div onClick={(e) => {
+        e.stopPropagation();
+        if(currentIndex === allCards.length - 1){
+          setCurrentIndex(0);
+        }else{
+          setCurrentIndex(currentIndex + 1)
+        }
+      }} className='w-12 h-12 p-2 hover:p-1 hover:border-2 rounded-full border border-black flex justify-center items-center absolute bottom-2 right-2 hover:text-2xl bg-green-500'><img src='/right-arrow.png' /></div>
+      <div onClick={(e) => {
+        e.stopPropagation();
+        if(currentIndex === 0){
+          setCurrentIndex(allCards.length - 1)
+        }else{
+          setCurrentIndex(currentIndex - 1)
+        }
+      }} className=' w-12 h-12 p-2 hover:p-1 hover:border-2 rounded-full border border-black flex justify-center items-center absolute bottom-2 left-2 hover:text-2xl z-10'><img src='/left-arrow.png' /></div>
+      <span className='text-4xl'>{frontOrBack ? allCards[currentIndex].frontText : allCards[currentIndex].backText}</span>
+    </motion.div>
   )
 }
 
--- pages/study.tsx
+++ pages/study.tsx
@@ -1,58 +1,19 @@
 import React, {useEffect, useState} from 'react'
 import StudyCards from '@/components/StudyCards'
 import { connectToDatabase } from '../mongoConnection';
+import {motion} from 'framer-motion';
 
 type Props = {}
 
 const study = ({ allCards }) => {
 
-
-  console.log('allCards: ', allCards);
-
-  // useEffect(() => {
-  //   const useEffectAsync = async () => {
-  //     const resp = await fetch('/api/cards');
-  //     const jsonResp = await resp.json();
-
-  //     setAllCards(jsonResp);
-  //   }
-
-  //   useEffectAsync();
-  // }, [])
-
   const [currentIndex, setCurrentIndex] = useState(0);
   // const [allCards, setAllCards] = useState([])
   const [frontOrBack, setFrontOrBack] = useState(true);
 
-  const changeSides = () => {
-    setFrontOrBack(!frontOrBack);
-  }
-
   return (
     <div className='w-full min-h-screen flex justify-center items-center'>
-        <div className='relative w-1/2 border border-black flex items-center h-80 rounded-lg justify-center' onClick={(e) => {
-          e.preventDefault();
-          changeSides();
-        }}>
-          <div className='absolute left-1 top-1'>
-            {currentIndex + 1}/{allCards.length}
-          </div>
-          <div onClick={() => {
-            if(currentIndex === allCards.length - 1){
-              setCurrentIndex(0);
-            }else{
-              setCurrentIndex(currentIndex + 1)
-            }
-          }} className='w-12 h-12 p-2 hover:p-1 hover:border-2 rounded-full border border-black flex justify-center items-center absolute bottom-2 right-2 hover:text-2xl bg-green-500'><img src='/right-arrow.png' /></div>
-          <div onClick={() => {
-            if(currentIndex === 0){
-              setCurrentIndex(allCards.length - 1)
-            }else{
-              setCurrentIndex(currentIndex - 1)
-            }
-          }} className=' w-12 h-12 p-2 hover:p-1 hover:border-2 rounded-full border border-black flex justify-center items-center absolute bottom-2 left-2 hover:text-2xl z-10'><img src='/left-arrow.png' /></div>
-          <span className='text-4xl'>{frontOrBack ? allCards[currentIndex].frontText : allCards[currentIndex].backText}</span>
-        </div>
+        <StudyCards allCards={allCards}/>
     </div>
   )
 }
